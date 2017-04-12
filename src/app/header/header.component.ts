7c351d2a63482c09f9d840854cad6de37a739e97
rajatkhare <rajat.khare@accenture.com>
2017-04-12 17:26:36 +0530

    Added components

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by rajat.khare on 12/04/17.
+ */
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'app-header',
+    templateUrl:'header.component.html'
+})
+
+export class HeaderComponent{
+
+}
