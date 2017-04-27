49ba60246ab230d52de67723c05a7b3f12d1d82a
rajatkhare <rajat.khare@accenture.com>
2017-04-27 17:52:16 +0530

    Added forms for shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,13 +2,12 @@
  * Created by rajat.khare on 17/04/17.
  */
 import {Recipe} from "./recipe.model";
-import {EventEmitter, Injectable} from "@angular/core";
+import { Injectable } from "@angular/core";
 import {Ingredient} from "../shared/ingredient.model";
 import {ShoppingListService} from "../shopping-list/shopping-list.service";
 
 @Injectable()
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
 
   constructor(private slService: ShoppingListService) {}
 
